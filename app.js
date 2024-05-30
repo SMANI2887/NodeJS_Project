c6c00b0f3ef0cd9b7670105ae5117603b1c22021
MANIKANDAN S <smani2887@gmail.com>
2024-05-30 12:58:31 +0530

    install cors and create middleware

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const express = require('express')
 // new express create app
 const app = express();
 
+// require cors
+const cors = require('cors');
+
+// use the cors middleware
+app.use(cors());
+
 // export the app
 module.exports = app;
