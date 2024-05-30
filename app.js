4142f058476685a08fba708eb3e5b4302915185c
MANIKANDAN S <smani2887@gmail.com>
2024-05-30 12:16:20 +0530

    start express server

---
+++ app.js
@@ -0,0 +1,9 @@
+// require express
+
+const express = require('express')
+
+// new express create app
+const app = express();
+
+// export the app
+module.exports = app;
--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const mongoose = require('mongoose');
 // import config
 const config = require('./utils/config') 
 
+// require app
+const app = require('./app')
+
 console.log('Connecting to MongoDB')
 // connect mongodb database
 mongoose.connect(config.MONGODB_URI)
 .then(() =>{
     console.log('Connected to MongoDB')
+
+    // Start THE server
+    app.listen(config.PORT, () => {
+        console.log(`server is running on port ${config.PORT}`)
+    })
+
 })
 .catch((error) => {
     console.log('Not connect to MongoDB :', error.message)
