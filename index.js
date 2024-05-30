9becc63dac5aa2fb7181b76385a8251decf5f158
MANIKANDAN S <smani2887@gmail.com>
2024-05-30 11:13:21 +0530

    Connected to MongoDB

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 // import config
 const config = require('./utils/config') 
 
-
-// import mongodb database
+console.log('Connecting to MongoDB')
+// connect mongodb database
 mongoose.connect(config.MONGODB_URI)
 .then(() =>{
-    console.log('connect to MongoDB')
+    console.log('Connected to MongoDB')
 })
 .catch((error) => {
     console.log('Not connect to MongoDB :', error.message)
